7130c2c2ecb84d3ac4f11feff41813c89d25ee3a
jayampathib <wmjb.weerasinghe@gmail.com>
2020-12-06 17:12:07 +0800

    1.7 add book edit /detail components

--- src/app/books/container/book-details/book-details.component.ts
+++ src/app/books/container/book-details/book-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../../../core/services/book.service';
+import { Book } from '../../../shared/models/book';
+//import { AngularConsoleLoggerService } from 'angular-console-logger';
+import { Observable } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-details',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent implements OnInit {
+  book$: Observable<Book> =this.activatedRoute.params.pipe(
+    map(p => +p.id),
+    switchMap(id => this.bookService.getSingle(id))
+  );
 
-  constructor() { }
+  constructor(
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly bookService: BookService,
+    //private readonly loggingService: AngularConsoleLoggerService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+ 
   }
 
+  deleteBook(book: Book) {
+    if (!confirm('really delete')) {
+      return;
+    }
+
+    this.bookService
+      .delete(book.id)
+      //.pipe(tap(() => this.loggingService.info('Somebody was deleting a book')))
+      .subscribe(() => {
+        this.router.navigate(['/books/overview/']);
+      });
+  }
 }
